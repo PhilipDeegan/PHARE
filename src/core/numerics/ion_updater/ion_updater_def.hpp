511464106e139567efddf05c3ce6a9aa3192b8d2
PhilipDeegan <philip.deegan@gmail.com>
2024-10-17 22:24:22 +0200

    ...

---
+++ src/core/numerics/ion_updater/ion_updater_def.hpp
@@ -0,0 +1,16 @@
+
+#ifndef PHARE_ION_UPDATER_DEF_HPP
+#define PHARE_ION_UPDATER_DEF_HPP
+
+#include <cstdint>
+
+
+namespace PHARE::core
+{
+
+enum class UpdaterMode : std::uint16_t { domain_only = 0, all };
+
+} // namespace PHARE::core
+
+
+#endif // ION_UPDATER_DEF_HPP
--- tests/core/data/vecfield/test_vecfield_fixtures.hpp
+++ tests/core/data/vecfield/test_vecfield_fixtures.hpp
@@ -1,17 +1,18 @@
 #ifndef PHARE_TEST_CORE_DATA_TEST_VECFIELD_FIXTURES_HPP
 #define PHARE_TEST_CORE_DATA_TEST_VECFIELD_FIXTURES_HPP
 
+#include "core/data/vecfield/vecfield.hpp"
 #include "tests/core/data/field/test_field_fixtures.hpp"
 #include "tests/core/data/tensorfield/test_tensorfield_fixtures.hpp"
 
 namespace PHARE::core
 {
 
-template<std::size_t dim>
-using VecField_t = VecField<Field_t<dim>, HybridQuantity>;
+template<std::size_t dim, auto alloc_mode = AllocatorMode::CPU>
+using VecField_t = VecField<Field_t<dim, alloc_mode>, HybridQuantity>;
 
-template<std::size_t dim>
-using UsableVecField = UsableTensorField<dim, /*rank=*/1>;
+template<std::size_t dim, auto alloc_mode = AllocatorMode::CPU>
+using UsableVecField = UsableTensorField<dim, /*rank=*/1, alloc_mode>;
 
 
 } // namespace PHARE::core
