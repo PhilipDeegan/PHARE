dd895393237693ecaa24f977048b2f3c07abccb1
PhilipDeegan <philip.deegan@gmail.com>
2025-06-04 18:12:12 +0200

    make gridlayout fns const (#1011)

--- src/core/data/grid/gridlayout.hpp
+++ src/core/data/grid/gridlayout.hpp
@@ -631,7 +631,7 @@ namespace core
          * if 'operand' is primal, the derivative is at the dual 'index' location
          */
         template<auto direction, typename Field>
-        NO_DISCARD auto deriv(Field const& operand, MeshIndex<Field::dimension> index)
+        NO_DISCARD auto deriv(Field const& operand, MeshIndex<Field::dimension> index) const
         {
             auto fieldCentering = centering(operand.physicalQuantity());
             using PHARE::core::dirX;
@@ -762,7 +762,7 @@ namespace core
          * on the dimensionality of the GridLayout.
          */
         template<typename Field>
-        NO_DISCARD auto laplacian(Field const& operand, MeshIndex<Field::dimension> index)
+        NO_DISCARD auto laplacian(Field const& operand, MeshIndex<Field::dimension> index) const
         {
             static_assert(Field::dimension == dimension,
                           "field dimension must be equal to gridlayout dimension");
