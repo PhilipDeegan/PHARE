7136054931f5bff981b0feb712cd91019162c7fb
deegan <philip.deegan@gmail.com>
2026-02-07 15:18:52 +0100

    hlld
    post rebase

--- tests/simulator/test_initialization.py
+++ tests/simulator/test_initialization.py
@@ -17,8 +17,8 @@ from pyphare.pharein.diagnostics import (
 )
 from pyphare.pharein.simulation import Simulation
 from pyphare.pharesee.geometry import level_ghost_boxes
-from pyphare.pharesee.hierarchy.hierarchy_utils import merge_particles
 from pyphare.pharesee.hierarchy import hierarchy_from
+from pyphare.pharesee.hierarchy.hierarchy_utils import merge_particles
 from pyphare.pharesee.particles import aggregate as aggregate_particles
 from pyphare.simulator.simulator import Simulator
 
@@ -267,7 +267,7 @@ class InitializationTest(SimulatorTest):
 
         from pyphare.pharein import global_vars
 
-        model = global_vars.sim.model
+        model = global_vars.sim.maxwellian_fluid_model
 
         bx_fn = model.model_dict["bx"]
         by_fn = model.model_dict["by"]
@@ -361,7 +361,7 @@ class InitializationTest(SimulatorTest):
 
         from pyphare.pharein import global_vars
 
-        model = global_vars.sim.model
+        model = global_vars.sim.maxwellian_fluid_model
         # protons and beam have same bulk vel here so take only proton func.
         vx_fn = model.model_dict["protons"]["vx"]
         vy_fn = model.model_dict["protons"]["vy"]
@@ -430,7 +430,7 @@ class InitializationTest(SimulatorTest):
 
         from pyphare.pharein import global_vars
 
-        model = global_vars.sim.model
+        model = global_vars.sim.maxwellian_fluid_model
         proton_density_fn = model.model_dict["protons"]["density"]
         beam_density_fn = model.model_dict["beam"]["density"]
 
@@ -503,7 +503,7 @@ class InitializationTest(SimulatorTest):
 
             from pyphare.pharein import global_vars
 
-            model = global_vars.sim.model
+            model = global_vars.sim.maxwellian_fluid_model
             density_fn = model.model_dict["protons"]["density"]
 
             patch = hier.level(0).patches[0]
